eb21328bee7de98efee2fdf77fc8a37caf3179de
Maheshbabu1204 <131903508+Maheshbabu1204@users.noreply.github.com>
2023-06-10 19:33:23 +0530

    Update temp.js

--- Task-3/temp.js
+++ Task-3/temp.js
@@ -1,1 +1,16 @@
-
+function convert() {
+    var temperature = document.getElementById("temperature").value;
+    var unit = document.getElementById("unit").value;
+    
+    if (unit === "fahrenheit") {
+    var celsius = (temperature - 32) * 5 / 9;
+    } else if (unit === "celsius") {
+    var fahrenheit = temperature * 9 / 5 + 32;
+    } else {
+    var kelvin = temperature + 273.15;
+    }
+    
+    document.getElementById("result").innerHTML = temperature + " " + unit + " = " + celsius + " degrees Celsius" + (unit === "fahrenheit" ? " or " + fahrenheit + " degrees Fahrenheit" : "");
+    }
+    
+    document.getElementById("convert").addEventListener("click", convert);
